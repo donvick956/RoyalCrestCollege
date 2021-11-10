ba73d189fa7577255369dbe9faee2c0237dfc550
victor okereke <donvick956@gmail.com>
2021-11-10 11:52:51 +0100

    Add project files.

---
+++ RoyalCrestCollege/ExamRecords.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultSystem
+{
+    class ExamRecords
+    {
+        public void Increment()
+        {
+            string path = @"c:\RoyalCrestCollege\studentrecord.txt";
+            int index = 0;
+            foreach (var item in File.ReadAllLines(path))
+            {
+
+                if (index < 20)
+                {
+                    var str = item.Split(" -");
+
+                    if (int.Parse(str[1]) >= (int)50)
+                    {
+                        var newIncrementedValue = int.Parse(str[1]) + 10;
+                        var newResult = $"{str[0]} - {newIncrementedValue}";
+                        File.AppendAllLines(@"c:\RoyalCrestCollege\newstudentrecord.txt", new String[] { newResult });
+                    }
+                    else
+                    {
+                        var newIncrementedValue = int.Parse(str[1]) + 5;
+                        var newResult = $"{str[0]} - {newIncrementedValue}";
+                        File.AppendAllLines(@"c:\RoyalCrestCollege\newstudentrecord.txt", new String[] { newResult });
+                    }
+                    index++;
+                }
+                continue;
+                
+            }
+        }
+    }
+}
---
+++ RoyalCrestCollege/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace ResultSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExamRecords FinalScores = new ExamRecords ();
+            FinalScores.Increment();
+         
+
+        }
+
+        
+
+        }
+
+
+    }
+
